8d96719ac2a0361d7a332e76d54b3f69f61a4694
Santiago Rocha <santiago.infsec@gmail.com>
2019-05-31 23:10:26 +0300

    Version 1 of extension to diplom

--- background.js
+++ background.js
@@ -1,6 +1,41 @@
-console.log('background running');
+'use strict';
 
-/*chrome.browserAction.onClicked.addListener(buttonClicked);
+const openLabelledId = "open-labelled";
+
+browser.menus.create({
+  id: openLabelledId,
+  title: "Check Phishing",
+  contexts: ["link"]
+});
+
+browser.menus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === openLabelledId) {
+    browser.tabs.update(tab.id, {
+      url: "https://www.virustotal.com/gui/home/url"
+    });
+  }
+});
+
+function updateMenuItem(linkHostname) {
+  browser.menus.update(openLabelledId, {
+    title: `Check phishing on (${linkHostname})`
+  });
+  browser.menus.refresh();
+}
+
+browser.menus.onShown.addListener(info => {
+  if (!info.linkUrl) {
+    return;
+  }
+  let linkElement = document.createElement("a");
+  linkElement.href = info.linkUrl;
+  updateMenuItem(linkElement.hostname);
+});
+
+
+/*console.log("background working");
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab) {
     let msg = {
@@ -8,12 +43,3 @@ function buttonClicked(tab) {
     }
     chrome.tabs.sendMessage(tab.id, msg);
 }*/
-
-chrome.runtime.onMessage.addListener(receiver);
-
-window.word = "coding train";
-
-function receiver(request, sender, sendResponse) {
-    console.log(request);
-    word = request.text;
-}
--- content.js
+++ content.js
@@ -1,27 +1,10 @@
-//console.log("Extension go?");
+console.log("Content working");
 
-/*chrome.runtime.onMessage.addListener(gotMessage);
+chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
     console.log(message.txt);
     if (message.txt == "running"){
-        let paragraphs = document.getElementsByTagName('p');
-        for (elt of paragraphs) {
-            elt.style['background-color'] = '#FF00FF';
-        }  
-
-    }
-}*/
-
-window.addEventListener('mouseup', wordSelected);
-
-function wordSelected() {
-    let selectedText = window.getSelection().toString().trim();
-    console.log(selectedText);
-    if (selectedText.length > 0) {
-        let message = {
-            text: selectedText
-        };
-        chrome.runtime.sendMessage(message);
+        alert("ready");
     }
 }
